365d30bf4000d95f9923e98deae3875bf4bb50e2
Raúl García <rulu158@gmail.com>
2023-08-06 19:13:59 +0200

    Added Section 6 Exercise 4

---
+++ Section6/Exercise4/employee_profile.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void employee_profile() {
+    
+    //----WRITE YOUR CODE BELOW THIS LINE----
+    
+    double hourly_wage {23.5};
+
+    string name {""};
+    cin >> name;
+    
+    int age {0};
+    cin >> age;
+    
+    //----WRITE YOUR CODE ABOVE THIS LINE----
+    //----DO NOT MODIFY THE CODE BELOW THIS LINE----
+    
+    cout << name << " " << age << " " << hourly_wage;
+}
